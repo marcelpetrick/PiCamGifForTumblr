cb75ef953c6f1ceaa921bcd79fc43f9779fa6d79
Marcel Petrick <mail@marcelpetrick.it>
2016-10-30 19:36:01 +0100

    20161030: logging-support; minor code-improvement

--- PiCamGifForTumblr.sh
+++ PiCamGifForTumblr.sh
@@ -2,15 +2,16 @@
 
 # author:	Marcel Petrick (mail@marcelpetrick.it)
 #
-# about:	Shoot photos, create a GIF and upload to tumblr.
+# about:        Shoot photos, create a GIF and upload to tumblr.
 #
 # license:	GNU General Public License v2.0
-# version:	0.1
+# version:	0.2
 # date:		20161030
 #
 # history:
-#		0.1 added support to use as much frames a possible while keeping the GIF-size inside the tumblr-animation-limit (max 25 frames)
-#		0.0 initial version
+#			0.2 logging-support; minor code-improvement
+#			0.1 added support to use as much frames a possible while keeping the GIF-size inside the tumblr-animation-limit (max 25 frames)
+#			0.0 initial version
 
 VERSION=0.1
 
@@ -21,6 +22,8 @@ if [ "$#" -eq  "0" ]; then
 	cd "${0%/*}" #go to the current directory of the script
 
 	FRAMES=25 # maximum amount of frames
+	OUTFILE="PiCam.log"
+	TEXT=`date`
 
 	# shoot some 500pix-wide photos and save them in a hardcoded directory (since it's run as cronjob)
 	echo "shoot now $FRAMES photos"
@@ -49,23 +52,30 @@ while true; do
 	# convert the single frames to an animated GIF
 	echo "gif now using $FRAMES frames"
 	gifsicle --delay=10 -O3 --loop $INPUTLIST > "$RESULTFILE"
+	#echo "$RESULTFILE"
 
 	#check the size of the GIF
 	if (( `stat -c%s "$RESULTFILE"` < $MAXSIZE )); then
 		echo "success: smaller than two mibyte with $FRAMES frames"
+		TEXT="$TEXT success $FRAMES frames"
 		break
 	else
 		echo "fail: bigger than two mibyte! -> next run with less frames (now $FRAMES)"
 		FRAMES=$((FRAMES-1)) # decrease by one .. ugly in bash
 		if (( "$FRAMES" <  0 )); then
 			echo "break because smaller zero"
+			TEXT="$TEXT fail"
 		fi
 	fi
 done #end of the loop: is exited via break
 
+	#logging
+	echo "$TEXT" >> "$OUTFILE"
+
 	# upload via the pre-configured pytumble-python-script
 	echo "upload now"
-	python /home/pi/Desktop/webcam/pytumble.py
+	python pytumble.py
+
 else
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # +++ just some totally unnecessary helper-functions +++
@@ -82,4 +92,3 @@ else
 		exit 1
 	fi
 fi
-
