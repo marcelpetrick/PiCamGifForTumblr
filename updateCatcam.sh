3c4c9663876470f895d467d993868eed9dbdb89b
Marcel Petrick <mail@marcelpetrick.it>
2022-02-22 12:14:24 +0100

    Update updateCatcam.sh

--- updateCatcam.sh
+++ updateCatcam.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# author:	Marcel Petrick (mail@marcelpetrick.it)
+# author:	mail@marcelpetrick.it
 
-mv /home/pi/Desktop/webcam/regulargif.sh /home/pi/Desktop/webcam/regulargif.sh1 
+
+mv /home/pi/Desktop/catcam/PiCamGifForTumblr.sh /home/pi/Desktop/catcam/PiCamGifForTumblr.sh1
 sudo rpi-update 
 sudo apt-fast update && sudo apt-fast dist-upgrade -y
 sudo apt-get autoclean
 sudo apt-get autoremove
-mv /home/pi/Desktop/webcam/regulargif.sh1 /home/pi/Desktop/webcam/regulargif.sh
+mv /home/pi/Desktop/catcam/PiCamGifForTumblr.sh1 /home/pi/Desktop/catcam/PiCamGifForTumblr.sh
 sudo reboot
-
